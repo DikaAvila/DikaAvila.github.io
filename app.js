e945ca71ad79cd301636ae8529a2f0e298509664
dika <muhammadikaavila@gmail.com>
2022-09-22 00:03:49 +0700

    penambahan link

--- app.js
+++ app.js
@@ -34,6 +34,13 @@ function pageTransition(){
             
         }
     })
+
+    //theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 pageTransition();
